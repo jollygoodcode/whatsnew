d8d9879ef335b6227daaa374f79149f1d422d05e
Juanito Fatas <katehuang0320@gmail.com>
2015-08-09 00:52:25 +0800

    Incorporate RemoteNewsFile in project.rb

--- lib/whatsnew/project.rb
+++ lib/whatsnew/project.rb
@@ -2,18 +2,22 @@ require "pathname"
 
 module Whatsnew
   class Project
+    attr_reader :news_file
+
     def initialize(path)
       @path = path
-      @name = Dir.glob(File.join(path, "*")).find do |file|
-        file =~ %r[(CHANGE|CHANGES|CHANGELOG|NEWS|HISTORY)]
-      end
+      @news_file = get_news_file_from_path
     end
 
-    def news_file
-      if @name
-        NewsFile.new(@name, project_uri)
+    def get_news_file_from_path
+      if path_is_a_sawyer_resource_array
+        RemoteNewsFile.new(path)
       else
-        NoNewsFile.new
+        if news_file_name
+          NewsFile.new(news_file_name, project_uri)
+        else
+          NoNewsFile.new
+        end
       end
     end
 
@@ -23,14 +27,26 @@ module Whatsnew
       if matched = matched_from_git_repository
         "https://#{matched[:host]}/#{matched[:owner]}/#{matched[:repo]}"
       else
-        "NOT FOUND"
+        "NOT FOUND".freeze
       end
     end
 
     private
 
+      attr_reader :path
+
+      def path_is_a_sawyer_resource_array
+        path.is_a?(Array) && path.first.is_a?(Sawyer::Resource)
+      end
+
+      def news_file_name
+        @news_file_name ||= Dir.glob(File.join(path, "*".freeze)).find do |file|
+          file =~ %r{(CHANGE|CHANGES|CHANGELOG|NEWS|HISTORY)}i.freeze
+        end
+      end
+
       def matched_from_git_repository
-        Dir.chdir(Pathname(@path).to_path) do
+        Dir.chdir(Pathname(path).to_path) do
           `git config --get remote.origin.url`.match(
             %r{git.+(?<host>(github.com|bitbucket.com|bitbucket.org))[:/](?<owner>\S+)/(?<repo>\S+)\.git}
           )
