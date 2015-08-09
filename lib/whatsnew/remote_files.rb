3ebd3cffb7da6516c9d4b8531cdff2b83aec8c34
Juanito Fatas <katehuang0320@gmail.com>
2015-08-09 15:38:06 +0800

    Cache news html url

--- lib/whatsnew/remote_files.rb
+++ lib/whatsnew/remote_files.rb
@@ -8,7 +8,7 @@ module Whatsnew
 
     def to_news_file
       if news
-        NewsFile.new(news.name, content: "See #{news.html_url}", file_url: news.html_url)
+        NewsFile.new(news.name, content: "See #{news_html_url}", file_url: news_html_url)
       else
         NoNewsFile.new
       end
@@ -17,5 +17,9 @@ module Whatsnew
     private
 
       attr_reader :news
+
+      def news_html_url
+        @news_html_url ||= news.html_url
+      end
   end
 end
