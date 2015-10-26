b7d76f8bae846e4cd410d0c7d9eac58738a07dc8
JuanitoFatas <katehuang0320@gmail.com>
2015-10-27 04:21:27 +0800

    Test Whatsnew::LocalFiles

--- lib/whatsnew/local_files.rb
+++ lib/whatsnew/local_files.rb
@@ -4,18 +4,11 @@ module Whatsnew
   class LocalFiles
     def initialize(path)
       @path = path
-      @news = Dir.glob(File.join(path, "*".freeze)).find do |file|
-        file =~ %r{(CHANGE|CHANGES|CHANGELOG|NEWS|HISTORY)}i.freeze
-      end
     end
 
     def to_news_file
-      if news
-        NewsFile.new(
-          file_name,
-          content: file.read,
-          file_url: "#{project_uri}/blob/master/#{file_name}"
-        )
+      if news = find_news_at_local
+        LocalNewsFile.new(news, path)
       else
         NoNewsFile.new
       end
@@ -23,30 +16,12 @@ module Whatsnew
 
     private
 
-      attr_reader :path, :news, :matched
+      attr_reader :path
 
-      def project_uri
-        if File.exist?("#{path}/.git") && matched_from_git_config
-          "https://#{matched[:host]}/#{matched[:owner]}/#{matched[:repo]}"
-        else
-          "NOT FOUND".freeze
+      def find_news_at_local
+        Dir.glob(File.join(path, "*".freeze)).find do |local_file|
+          local_file =~ %r{(CHANGE|CHANGES|CHANGELOG|NEWS|HISTORY)}i.freeze
         end
-      end
-
-      def matched_from_git_config
-        @matched ||= Dir.chdir(Pathname(path).to_path) do
-          `git config --get remote.origin.url`.match(
-            %r{(http://|https://|git.+)(?<host>(github.com|bitbucket.com|bitbucket.org))[:/](?<owner>\S+)/(?<repo>\S+)\.git}
-          )
-        end
-      end
-
-      def file
-        @file ||= Pathname(news)
-      end
-
-      def file_name
-        @file_name ||= file.basename.to_s
       end
   end
 end
